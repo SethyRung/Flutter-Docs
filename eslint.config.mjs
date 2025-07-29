eedd2023270a07a6df894371a1b02565c2b970d2
Sethy Rung <rungsethyhk@gmail.com>
2025-07-29 20:41:51 +0700

    feat: init project

---
+++ eslint.config.mjs
@@ -0,0 +1,16 @@
+import withNuxt from "./.nuxt/eslint.config.mjs";
+
+export default withNuxt({
+  rules: {
+    "vue/html-self-closing": [
+      "warn",
+      {
+        html: {
+          void: "always",
+          normal: "never",
+        },
+      },
+    ],
+    "vue/multi-word-component-names": ["naver"],
+  },
+});
---
+++ nuxt.config.ts
@@ -0,0 +1,33 @@
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineNuxtConfig({
+  future: {
+    compatibilityVersion: 4,
+  },
+  compatibilityDate: "2024-11-27",
+  devtools: { enabled: true },
+  css: ["~/assets/css/main.css"],
+  modules: [
+    "@nuxt/ui",
+    "@nuxt/eslint",
+    "@pinia/nuxt",
+    "@vueuse/nuxt",
+    "dayjs-nuxt",
+    "@nuxtjs/google-fonts",
+  ],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  colorMode: {
+    preference: "light",
+  },
+  googleFonts: {
+    download: true,
+    families: {
+      Inter: [100, 200, 300, 400, 500, 600, 700, 800, 900],
+      "Red Hat Display": [300, 400, 500, 600, 700, 800, 900],
+      "Kantumruy Pro": [100, 200, 300, 400, 500, 600, 700],
+      Battambang: [100, 300, 400, 700, 900],
+    },
+  },
+});
