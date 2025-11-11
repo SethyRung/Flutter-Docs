4b33d08d57eae9a89a70c0629bbfb3c0888b151e
Sethy Rung <rungsethyhk@gmail.com>
2025-11-11 20:58:41 +0700

    feat(docs): enhance UPageHeader with dynamic links and description rendering

--- app/pages/docs/[...slug].vue
+++ app/pages/docs/[...slug].vue
@@ -37,14 +37,25 @@ useSeoMeta({
 
 <template>
   <UPage v-if="page">
-    <UPageHeader :title="page.title">
-      <template #description>
-        <MDC
-          v-if="page.description"
-          :value="page.description"
-          unwrap="p"
-          :cache-key="`${kebabCase(route.path)}-description`"
-        />
+    <UPageHeader :title="page.title" :description="page.description">
+      <template #links>
+        <UButton
+          v-for="link in page.links"
+          :key="link.label"
+          color="neutral"
+          variant="outline"
+          :target="link.to?.startsWith('http') ? '_blank' : undefined"
+          v-bind="link"
+        >
+          <template v-if="link.avatar" #leading>
+            <UAvatar
+              v-bind="link.avatar"
+              size="2xs"
+              :alt="`${link.label} avatar`"
+            />
+          </template>
+        </UButton>
+        <PageHeaderLinks />
       </template>
     </UPageHeader>
 
