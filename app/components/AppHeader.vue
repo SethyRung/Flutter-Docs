801592b0300f3fbc629cc3044271a5fa804dde06
Sethy Rung <rungsethyhk@gmail.com>
2025-12-13 10:37:56 +0700

    feat: update header links

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -17,12 +17,9 @@ const isDark = computed({
   <UHeader mode="slideover" title="Flutter">
     <template #title>
       <ClientOnly>
-        <UAvatar
-          :src="isDark ? '/images/flutter-white.svg' : '/images/flutter.svg'"
-          :ui="{
-            root: 'w-full h-6 rounded-none bg-transparent',
-          }"
-        />
+        <UAvatar :src="isDark ? '/images/flutter-white.svg' : '/images/flutter.svg'" :ui="{
+          root: 'w-full h-6 rounded-none bg-transparent',
+        }" />
       </ClientOnly>
     </template>
 
@@ -36,25 +33,14 @@ const isDark = computed({
       </UTooltip>
 
       <UTooltip text="Open on GitHub" :kbds="['meta', 'G']">
-        <UButton
-          color="neutral"
-          variant="ghost"
-          to="https://github.com/SethyRung/Flutter-Docs"
-          target="_blank"
-          icon="i-simple-icons-github"
-          aria-label="GitHub"
-        />
+        <UButton color="neutral" variant="ghost" to="https://github.com/SethyRung/Flutter-Docs" target="_blank"
+          icon="i-simple-icons-github" aria-label="GitHub" />
       </UTooltip>
     </template>
     <template #body>
-      <UNavigationMenu
-        orientation="vertical"
-        variant="link"
-        :items="headerLinks"
-        :ui="{
-          root: '-mx-2.5 -mt-1.5',
-        }"
-      />
+      <UNavigationMenu orientation="vertical" variant="link" :items="headerLinks" :ui="{
+        root: '-mx-2.5 -mt-1.5',
+      }" />
     </template>
   </UHeader>
 </template>
